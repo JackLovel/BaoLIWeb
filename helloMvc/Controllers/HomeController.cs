43d0338e3a4c05d982f7312db349d6e6ddf60c1c
JackLovel <2101721230@qq.com>
2019-04-16 18:48:34 +0800

    add some clidren page

--- helloMvc/Controllers/HomeController.cs
+++ helloMvc/Controllers/HomeController.cs
@@ -4,13 +4,23 @@ namespace helloMvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult SignUp() {
+            return View();
+        }
+
+        public IActionResult ContactUs() {
+            return View();
+        }
+
+        public IActionResult BuyTicket() {
+            return View();
+        }
+
+        public IActionResult KnowMore() {
             return View();
         }
     }
