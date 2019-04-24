d0908a308c02976620c7b66373d6665d5d23ac31
JackLovel <2101721230@qq.com>
2019-04-24 11:07:51 +0800

    fix fixed-top

--- helloMvc/Controllers/HomeController.cs
+++ helloMvc/Controllers/HomeController.cs
@@ -9,10 +9,17 @@ namespace helloMvc.Controllers
         }
 
         public IActionResult SignUp() {
+            ViewData["item1"] = "this is a item1, but is a test..";
+            ViewData["item2"] = "this is a item2, but is a test..";
+            ViewData["item3"] = "this is a item3, but is a test..";
+            ViewData["item4"] = "this is a item4, but is a test..";
+            
             return View();
         }
 
         public IActionResult ContactUs() {
+            ViewData["qqGroup"] = "2123131204123";
+
             return View();
         }
 
@@ -21,6 +28,14 @@ namespace helloMvc.Controllers
         }
 
         public IActionResult KnowMore() {
+            return View();
+        }
+
+        public IActionResult ProductInformation() {
+            return View();
+        }
+		
+        public IActionResult test() {
             return View();
         }
     }
