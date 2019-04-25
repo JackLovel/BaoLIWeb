e7de0fab823fd87c1773d46ffd65af6fc1626005
JackLovel <2101721230@qq.com>
2019-04-25 19:23:00 +0800

    update

--- helloMvc/Controllers/HomeController.cs
+++ helloMvc/Controllers/HomeController.cs
@@ -5,6 +5,17 @@ namespace helloMvc.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() {
+
+            // 赞助商支持 表格 数据
+            ViewData["table_col_two"] = "名字";
+            ViewData["table_col_three"] = "职位";
+
+            ViewData["table_row_one_role"] = "网站制作及布署";
+            ViewData["table_row_one_name"] = "叶落初冬";
+
+            ViewData["table_row_two_role"] = "美工";
+            ViewData["table_row_two_name"] = "蹄铁";
+
             return View();
         }
 
